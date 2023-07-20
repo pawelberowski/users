0390770a5734d7ed13676612cb99dc8e7dc5e2d9
Taz <pawelberowski@gmail.com>
2023-07-20 03:54:30 +0200

    Added renderCompaniesTable function

--- app/index.js
+++ app/index.js
@@ -2,9 +2,11 @@ import './styles/styles.scss';
 import { getUsersData } from './getUsersData';
 import { getCompaniesData } from './getCompaniesData';
 import { renderUsersTable } from './renderUsersTable';
+import { renderCompaniesTable } from './renderCompaniesTable';
 
 window.onload = () => {
   renderUsersTable();
+  renderCompaniesTable();
 };
 getUsersData().then((data) => console.log(data));
 getCompaniesData().then((data) => console.log(data));
