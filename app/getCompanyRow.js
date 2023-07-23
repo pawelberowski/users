814d39ffafe2e40bcfc8807f9a602fca7dc8f431
Taz <pawelberowski@gmail.com>
2023-07-23 07:08:30 +0200

    Removed unused files

--- app/getCompanyRow.js
+++
@@ -1,7 +0,0 @@
-export function getCompanyRow(name) {
-  const row = document.createElement('tr');
-  const nameData = document.createElement('td');
-  nameData.innerText = `${name}`;
-  row.append(nameData);
-  return row;
-}
--- app/getUserRow.js
+++
@@ -1,9 +0,0 @@
-export function getUserRow(name, email) {
-  const row = document.createElement('tr');
-  const nameData = document.createElement('td');
-  const emailData = document.createElement('td');
-  nameData.innerText = `${name}`;
-  emailData.innerText = `${email}`;
-  row.append(nameData, emailData);
-  return row;
-}
